76002930e8e3975e1b8508fcd32fa25d2fea649e
Retro30 <166532470+ebcfdfgd@users.noreply.github.com>
2026-04-08 00:29:44 +0200

    Delete Smw hack directory

--- Smw hack/readme.txt
+++
@@ -1,5 +0,0 @@
-rom name is Super Mario World (USA).sfc 
-
-add header to rom by tush  
-
-then apply patch with floating
--- Smw hack/tush for header rom/src/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef INC_MAIN_H
-#define INC_MAIN_H
-
-#define msgBox(message, title, style) wxMessageBox(wxT(message), wxT(title), style)
-
-#endif /* INC_MAIN_H */
-
